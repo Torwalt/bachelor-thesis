e1760fd8dab09b8d9e3ee145275d1757178adf54
Alexander Dadiani <alexdadiani1994@gmail.com>
2019-02-06 21:47:22 +0100

    Work in progress. Added new algorithms. This commit is only for backup purposes.

--- latex/cleardir.bat
+++
@@ -1,5 +0,0 @@
-echo off
-title Clear Directory
-echo Starting cleaning process... 
-rm -f *.aux *.bbl *.blg *.log *.out *.pdf *.lof *.lol *.lot *.toc *.ps *.parabola.* *.loa *.synctex.gz
-echo Cleaning process fininshed!
--- latex/makefast.bat
+++
@@ -1,4 +0,0 @@
-echo off
-
-echo FAST Making new pdf...
-pdflatex -shell-escape thesis-main.tex
